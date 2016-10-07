bff5efa712082deb7b4c3c96c1429a11ac395786
Jbee <Jbee@AL01155703.local>
2016-10-07 17:26:33 +0900

    UserController Add

---
+++ mojito/src/main/java/com/mojito/web/UserController.java
@@ -0,0 +1,17 @@
+package com.mojito.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+	@GetMapping("/login")
+	public String login(){
+		return "login_page";
+	}
+	
+	@GetMapping("/user")
+	public String signup(){
+		return "signup_page";
+	}
+}
