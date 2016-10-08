0bcda05188e9a76eba4699481e053f2adf1f4633
wkddngus5 <wkddngus5@naver.com>
2016-10-08 22:14:12 +0900

    Add files via upload

---
+++ 2016-3-MOJITO_WEB/mojito/src/main/resources/static/css/2016-3-MOJITO_WEB/mojito/src/main/java/com/mojito/MojitoApplication.java
@@ -0,0 +1,12 @@
+package com.mojito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MojitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MojitoApplication.class, args);
+	}
+}
