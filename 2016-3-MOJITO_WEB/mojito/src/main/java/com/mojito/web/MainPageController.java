719dd981dcd04568155f5e6c1747fb0a5ac34aa0
wkddngus5 <wkddngus5@naver.com>
2016-10-08 22:26:12 +0900

    Add files via upload

---
+++ 2016-3-MOJITO_WEB/mojito/src/main/java/com/mojito/web/MainPageController.java
@@ -0,0 +1,12 @@
+package com.mojito.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainPageController {
+	@GetMapping("/")
+	public String mainPage(){
+		return "main_page";
+	}
+}
