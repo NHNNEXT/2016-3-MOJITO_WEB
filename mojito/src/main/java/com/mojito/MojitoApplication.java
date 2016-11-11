f93bde69a3d4193533e982388a6ebfc36a6b5fff
Jbee <Jbee@AL01155703.local>
2016-11-11 15:35:23 +0900

    필요없는 import 삭

--- mojito/src/main/java/com/mojito/MojitoApplication.java
+++ mojito/src/main/java/com/mojito/MojitoApplication.java
@@ -3,7 +3,6 @@ package com.mojito;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.service.ApiInfo;
