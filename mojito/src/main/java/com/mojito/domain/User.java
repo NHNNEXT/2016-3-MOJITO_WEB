745803e62c2fe9e180bb5057a5fd0417dbcb76d4
Jbee <ljyhanll@gmail.com>
2016-11-11 11:35:41 +0900

    Feature/friend list (#6)
    * Update README.md
    
    * create_article
    
    * feature/login #로그인, 회원가입, 개인 정보 수정 기능 구현 (#4)
    
    * 기본 로그인 & 로그아웃 구현. 상단 navigator 로그인 여부에 따라 보이는 메뉴 다르게함.
    => 훌륭합니다!
    
    * 상단 nav 간소화. 로그인 사용자 이름을 표시하고 "새 모임" 이외의 기능은 drop down 메뉴에 집어 넣음
    => 좋네요~
    
    * 우현이 commit 땡겨오기 전 중간 커밋 - login 기능 세부 구현 중 (사용자를 구분 해 개인 정보 수정, 로그인 실패 원인에 따라 다른 로그인 화면으로 리다이렉트 등)
    => 로그인 실패 원인이 드러나나요?
    
    * 회원 가입 기능, 개인 정보 수정 기능(password confirm 과정 포함) 구현
    => password confirm은 아직 다 구현이 안된건가요?
    1) 패스워드와 확인용 패스워드가 일치하지 않을 경우 처리
    2) 비밀번호 변경은 하지 않고 다른 부분만 변경할 때, 비밀번호 입력 공간을 빈 공간으로 했을 경우, 비밀번호가 공백으로 리셋되는지 확인. ( 제 생각에는 이 상태이면 공백으로 리셋될 것 같네요. ) 비밀번호가 입력되었는지 확인을 하고, 입력되어 있지 않으면 비밀번호는 update 하지 않아야 할 것 같습니다.
    
    나중에 로직을 추가할 때 참고하세요~
    
    * User끼리에서 발생할 수 있는 4가지 경우에 대한 관계 추가.
    1) 현재 내 친구 목록
    2) 나에게 온 친구 요청 목록
    3) 내가 요청 중인 친구 목록
    4) 모임에서 만난 사람 목
    
    * 관계추가에 따른 테스트 코드 추가
    
    * html/css 수정 및 보완
    - friend_list_page : 추가된 리스트에 대해 보완
    - 나머지 html : footer 제
    
    * swagger library 추가
    
    * 로고에 hover 시 #000 으로 설정되어있던 css 삭제
    
    * dropMenu a태그와 li 태그 위치를 변경 ( swap ) 클릭하기 편하도록 바꿈
    a태그가 밖에 있어야 클릭 범위가 넓어짐
    
    * User 객체의 name을 userName으로 변경함
    그에 따른 테스트 코드 변경 및 다른 코드 변경 사항 적용
    또한 lazyInitialization 문제가 발생하여 transaction을 따로 설정하는 방법 말고 fetchType을 EAGER로 변경.
    추후 transaction 을 사용하는 방법으로 변경할 예정.
    (lazyInitialization 문제는 따로 정리해서 올릴 예정)
    
    * 모바일 백엔드 테스트를 위해 JSON 형식으로 Login 컨트롤러를 구현
    추후 웹과 합칠 수 있는 방법이 있다면 그쪽으로 변경할 예정.
    
    추가적으로 ajax을 이용하여 User 객체에 있는 list 중 하나를 불러오는 컨트롤러 생성.
    아직 불안정한 상태.
    이 부분에서 lazyinitialization 문제가 발생함.
    
    * 추가적인 작업을 위해서 html / css 파일 수정 .
    
    * 내가 친구 요청한 리스트를 불러오는 ajax 구현 javascript code .
    this와의 전쟁에서 패배함.
    
    현재 발생하고 있는 문제
    1) 요청하고 있는 친구 리스트가 없는 경우를 대비하여 if 문으로 분기하여 다른 html 태그를 보여주려고 했지만 이상하게 non-list 부분은 html 코드 조각이 삽입되지 않는다.
    2) else 뒤에 부분은 제대로 수행되는 듯 하지만, 한 번 클릭되어 생성되면 더 이상 생성되지 않아야 하는데, 계속 생성되서 밑으로 달림.
    3) 리스트에 맞춰서 height 길이를 조정하려 했지만 아직 구현하지 못함
    
    위 세 가지 문제들을 추후 해결할 예정.
    최대한 깔끔하게 javascript 코드를 작성하려 했으나 로직에서부터 막혀 refactoring을 진행하지 못함.
    
    * no message

--- mojito/src/main/java/com/mojito/domain/User.java
+++ mojito/src/main/java/com/mojito/domain/User.java
@@ -1,22 +1,44 @@
 package com.mojito.domain;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class User {
 	@Id
 	@GeneratedValue
+    @JsonProperty
 	private Long id;
 
 	@Column(nullable = false, unique=true, length=40)
+    @JsonProperty
 	private String userEmail;
-	
+
 	@Column(nullable = false)
+    @JsonProperty
 	private String userPassword;
 	private String name;
+
+    @JsonProperty
+    private String userName;
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JsonProperty
+    private Set<User> friendUsers;
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JsonProperty
+    private Set<User> requestsToUser;
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JsonProperty
+    private Set<User> requestsToMe;
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JsonProperty
+    private Set<User> metUsers;
 
 	public String getUserEmail() {
 		return userEmail;
@@ -30,20 +52,23 @@ public class User {
 		this.userPassword = userPassword;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void setUserName(String userName) {
+		this.userName = userName;
 	}
-	
-	@Override
+
+    public Set<User> getRequestsToUser() {
+        return requestsToUser;
+    }
+
+    @Override
 	public String toString() {
-		return "User [id=" + id + ", userEmail=" + userEmail + ", userPassword=" + userPassword + ", name=" + name + "]";
+		return "User [id=" + id + ", userEmail=" + userEmail + ", userPassword=" + userPassword + ", userName=" + userName + "]";
 	}
 
 	public boolean matchPassword(User newUser) {
 		if (newUser.userPassword == null) {
 			return false;
 		}
-		
 		return newUser.userPassword.equals(userPassword);
 	}
 
---
+++ mojito/src/main/resources/static/js/login_snackbar.js
@@ -0,0 +1,5 @@
+$("#loginForm").keyup(function(e) {
+	if (e.keyCode == 13) {
+		$("#demo-show-snackbar").click();
+	}
+});
