d563cefe67a67e278f06017bbd706fdd99466d81
brain <wkddngus5@naver.com>
2016-12-04 22:00:16 +0900

    mustache로 내 만남 보여주기

--- mojito/src/main/java/com/mojito/web/MeetingController.java
+++ mojito/src/main/java/com/mojito/web/MeetingController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,7 +37,8 @@ public class MeetingController {
 	}
 	
 	@GetMapping("/my")
-	public String my(){
+	public String my(Model model){
+		model.addAttribute("meetings", meetingRepository.findAll());
 		return "my_meeting_page";
 	}
 	
