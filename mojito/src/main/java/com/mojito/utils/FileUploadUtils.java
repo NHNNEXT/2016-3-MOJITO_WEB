fa1537f7684fefab7f0d2791d8ca7e2a4676ea01
brain <wkddngus5@naver.com>
2016-12-06 21:14:26 +0900

    recommenders 제거2

---
+++ mojito/src/main/java/com/mojito/utils/FileUploadUtils.java
@@ -0,0 +1,24 @@
+package com.mojito.utils;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class FileUploadUtils {
+	public static final String filePath = "/Users/Naver/Desktop"; // 프로젝트 외부 경로
+//	public static final String filePath = "./src/main/resources/static/image/"; // 프로젝트 내부 경로
+	
+    public static final String fileUpload(MultipartFile uploadFile) throws IOException {
+        String fileName = uploadFile.getOriginalFilename();
+
+        File file = new File(filePath + fileName); //path
+        System.out.println(file);
+        uploadFile.transferTo(file);
+
+        return fileName;
+    }
+}
